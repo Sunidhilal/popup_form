81c3c68c51d205b85a5ddb2371032a08f3f3fe7c
Sunidhi <lal.sunidhi21@gmail.com>
2019-04-03 16:24:17 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Popup} from 'ng2-opd-popup';
+import {NgModule} from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { from } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'popUp';
+export class AppComponent{
+  public show:boolean=false;
+  form:FormGroup;
+  submitted=false;
+  loading=false;
+  isDisabled1=true;
+  isDisabled2=true;
+  isChecked:boolean=false;
+  WebsiteName: any;
+  public hitCancel:boolean=false;
+  
+  constructor(private popup:Popup,private formbuilder:FormBuilder, private http: HttpClient){}
+ 
+  checkEmpty() {
+     if (this.WebsiteName)
+        this.isDisabled2=false;
+}
+cancelButton()
+{
+   this.hitCancel=true;
+}
+register(){
+  let options={
+    headers: new HttpHeaders().set('Content-Type', 'application/JSON')
+  };
+}
+onSubmit = function (user: any) {
+  console.log(user);
+ // var obj = JSON.parse(user);
+  //console.log(obj);
+  this.http.post("http://10.12.10.134:8080/cookies", user).subscribe();
+  
+  //this.http.post('http://localhost:4200', user);
+}
+  ToggleButton1(){
+     this.isChecked=!(this.isChecked);
+     console.log(this.isChecked);
+     if(this.isChecked==true)
+       this.isDisabled1=false;
+     if(this.isChecked==false)
+       this.isDisabled1=true;
+   }
+   toggle(){
+     this.show=!this.show;
+   }
+    ClickButton(){
+      this.popup.options={
+        header:"Add Website",
+        showButtons: false,
+      }
+      this.popup.show();
+      this.popup.show(this.popup.options);
+    }
+  ngOnInit(){
+    this.register();
+    var h:boolean;
+    if(this.isChecked==true)
+       h=true;
+     else
+       h=false;
+       console.log(h);
+    this.form=this.formbuilder.group({
+      domain: ['', Validators.required],
+      //org: ['', Validators.required],
+      pages:[ Validators.required]
+    });
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {PopupModule} from 'ng2-opd-popup';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PopupModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
